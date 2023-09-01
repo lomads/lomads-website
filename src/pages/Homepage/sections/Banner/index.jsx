cd883b5c6e7321fcd51ae7876b38a424593edb97
Lomads Technologies <107495745+lomads@users.noreply.github.com>
2023-09-01 16:25:51 +0200

    Update index.jsx

--- src/pages/Homepage/sections/Banner/index.jsx
+++ src/pages/Homepage/sections/Banner/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Banner.style.css';
 
+import hero from '../../../../assets/HERO.svg';
 import pill1 from '../../../../assets/pill1.svg';
 import pill2 from '../../../../assets/pill2.svg';
 
@@ -16,9 +17,9 @@ const Banner = () => {
         <section className="banner-container">
             <div className="heading-section">
                 <div className="heading-container">
-                    <h1 data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="animate__animated animate__fadeIn animate__fast">WHERE CAPITAL</h1>
-                    <h1 data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="animate__animated animate__fadeIn animate__fast">AND PEOPLE <span>TANGO</span></h1>
-                    <h1 data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="animate__animated animate__fadeIn animate__fast"><span>TO GREATNESS</span></h1>
+                    <h1 data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="animate__animated animate__fadeIn animate__fast"> <br /> </h1>
+                    <h1 data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="animate__animated animate__fadeIn animate__fast">CommandCentral<span></span></h1>
+                    <h1 data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="animate__animated animate__fadeIn animate__fast">for <span>Collectives</span></h1>
                 </div>
                 <div data-speed={window.innerWidth > 1024 ? "0.8" : "1"} className="heading-pill pos1 animate__animated animate__bounceIn animate__delay-2s animate__faster">
                     <img src={pill1} alt="pill1" />
@@ -29,11 +30,11 @@ const Banner = () => {
                     <span>+1</span>
                 </div>
             </div>
-            <p data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="animate__animated animate__fadeIn animate__delay-1s">Achieve unprecedented efficiency while empowering members<br /> of your organization with personal data sovereignty</p>
-            <button data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="access-btn animate__animated animate__fadeIn animate__delay-1s" onClick={() => { window.location.href = "/earlyAccess" }}>GET EARLY ACCESS <HiOutlineArrowRight size={20} /></button>
+            <p data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="animate__animated animate__fadeIn animate__delay-1s"><br /> Lomads Offers A Crisp Framework To Streamline And Formalize Operations</p>
+            <button data-speed={window.innerWidth > 1024 ? "1.2" : "1"} className="access-btn animate__animated animate__fadeIn animate__delay-1s" onClick={() => { window.location.href = "/earlyAccess" }}>JOIN OUR WAITLIST <HiOutlineArrowRight size={20} /></button>
 
             <div id="hero-section" className="hero-section animate__animated animate__fadeInUp animate__slow">
-                <img src="https://raw.githubusercontent.com/lomads/lomads-website/main/src/assets/HERO.svg" className="hero-img" alt="hero" data-speed="1" />
+                <img src={hero} className="hero-img" alt="hero" data-speed="1" />
                 <img src={box1} className="whitebox pos1" alt="box1" data-speed={window.innerWidth > 1024 ? "1.2" : "1"} />
                 <img src={box2} className="whitebox pos2" alt="box2" data-speed={window.innerWidth > 1024 ? "1.5" : "1"} />
                 <img src={box3} className="whitebox pos3" alt="box3" data-speed={window.innerWidth > 1024 ? "1.2" : "1"} />
