e9eae04ab6a8292d271aaddb39476e3d31e26afc
Lomads Technologies <107495745+lomads@users.noreply.github.com>
2023-09-01 17:37:04 +0200

    Update index.jsx

--- src/pages/Homepage/sections/Dashboard/index.jsx
+++ src/pages/Homepage/sections/Dashboard/index.jsx
@@ -17,7 +17,6 @@ const Dashboard = () => {
             <div className="dashboard-header">
                 <h1>ONE <span>DASHING</span><br />DASHBOARD</h1>
             </div>
-
             <div className="dashboard-text-container" style={{"width" : "70%"}}>
             {
                  <div className="dashboard-text pos1">
@@ -26,9 +25,6 @@ const Dashboard = () => {
                 </div>}
                 
             </div>
-
-            <div className="dashboard-text-container">
-      
 
             <div className="dashboard-body">
                 <img className="imgSm" src={dashboardsm} alt="" />
@@ -42,8 +38,19 @@ const Dashboard = () => {
                 {/* <img className="imgMilestone" src={milestonesm} alt="" /> */}
             </div>
 
+            <div className="dashboard-text-container">
+            {/*
+                 <div className="dashboard-text pos1">
+                    <h1>Easy information access</h1>
+                    <p>The Lomads dashboard is expertly designed to help you focus on tasks that matter,<span> without overwhelming you with information.</span></p>
+                </div>
 
-
+                <div className="dashboard-text pos2">
+                    <h1>Efficient workflow automation</h1>
+                    <p>Our in-built automations help you save time by eliminating repetitive tasks. With just a click, a project milestone can be approved and in just a snap, a transaction is created for the respective team, automatically. <span>Sleek OP!</span></p>
+                </div>
+                
+                */}
             </div>
 
             <img className="imgMilestone" src={milestonesm} alt="" />
