71d1b704ba7d14bb638f56687edb09ff95d1e13a
Lomads Technologies <107495745+lomads@users.noreply.github.com>
2023-09-22 10:51:00 +0200

    Update index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,10 +3,11 @@ import "./Navbar.style.css";
 
 import logo from "../../assets/logo.svg";
 import logolight from "../../assets/logo-light.svg";
-// import menu from '../../assets/menu.svg';
+import menu from '../../assets/menu.svg';
 
 import { IoCloseOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
+
 
 /**
  *
@@ -14,11 +15,11 @@ import { Link } from "react-router-dom";
  * @returns
  */
 const Navbar = ({ useBlogLogo }) => {
-  // const handleSlideDownAnimation = () => {
-  //     const menu = document.getElementById('full-screen-menu');
-  //     menu.classList.remove('slideUp');
-  //     menu.classList.add('slideDown');
-  // }
+       const handleSlideDownAnimation = () => {
+       const menu = document.getElementById('full-screen-menu');
+       menu.classList.remove('slideUp');
+       menu.classList.add('slideDown');
+   }
 
   const handleSlideUpAnimation = useCallback(() => {
     const menu = document.getElementById("full-screen-menu");
@@ -35,8 +36,19 @@ const Navbar = ({ useBlogLogo }) => {
         </Link>
         {useBlogLogo && <p className="blog-logo">Blog</p>}
         </div>
+        
 
         <div style={{ display: "flex", alignItems: "center" }}>
+
+        <a
+            className="menu-p"
+            href="https://www.notion.so/lomads/Lomads-Key-Features-Roadmap-0f0fbc49d063436f95c97f26c57479d8"
+            target="_blank"
+            rel="noreferrer"
+          >
+            FEATURES
+          </a>
+
           <Link className="menu-p" to="/blog">
             BLOG
           </Link>
@@ -49,7 +61,7 @@ const Navbar = ({ useBlogLogo }) => {
           >
             DOCS
           </a>
-          {/* <button onClick={handleSlideDownAnimation}><img className="menu" src={menu} alt="logo" /></button> */}
+          <button onClick={handleSlideDownAnimation}><img className="menu" src={menu} alt="logo" /></button>
         </div>
       </section>
 
@@ -63,30 +75,40 @@ const Navbar = ({ useBlogLogo }) => {
         </div>
         <div className="menu-contents">
           <div className="menu-row">
-            <h1>
-              <span>What</span> we do?
-            </h1>
+          <a
+            className="menu-p"
+            href="https://www.notion.so/lomads/Lomads-Key-Features-Roadmap-0f0fbc49d063436f95c97f26c57479d8"
+            target="_blank"
+            rel="noreferrer"
+          >
+          <h1>
+              FEATURES
+          </h1>
+          </a>
+
           </div>
           <div className="menu-row">
-            <h1>
-              <span>How</span> we do it?{" "}
-            </h1>
+          <Link className="menu-p" to="/blog">
+          <h1>
+              BLOG
+          </h1>
+          </Link>
           </div>
           <div className="menu-row">
-            <h1>
-              Our <span>partners</span>
-            </h1>
+          <a
+            className="menu-p"
+            href="https://lomads-1.gitbook.io/lomads/"
+            target="_blank"
+            rel="noreferrer"
+          >
+          <h1>
+              DOCS
+          </h1>
+          </a>
+  
           </div>
-          <div className="menu-row">
-            <h1>
-              Lomads <span>and you</span>
-            </h1>
-          </div>
-          <div className="menu-row">
-            <h1>
-              <span>Contact</span> us
-            </h1>
-          </div>
+
+
         </div>
       </div>
     </>
